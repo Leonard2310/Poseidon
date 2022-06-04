069be7c5f87010b121ffc05fadb6c95d08f30e76
Leonardo Catello <leonardo.catello23@gmail.com>
2022-06-04 17:58:56 +0200

    GUI v1.0.1

---
+++ src/poseidon/main/LaunchMenu.java
@@ -0,0 +1,70 @@
+package poseidon.main;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.border.Border;
+
+public class LaunchMenu {
+	
+	JFrame frame = new JFrame();
+	JLabel label = new JLabel();
+	
+	LaunchMenu() {
+		
+		ImageIcon icon = new ImageIcon("icon.jpeg");
+		Border border = BorderFactory.createLineBorder(Color.BLUE,4);  
+		
+		// LABEL
+		JLabel label = new JLabel();
+		label.setText("POSEIDON");
+		label.setBounds(0,0,150,150);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		label.setVerticalTextPosition(JLabel.TOP);
+		label.setForeground(new Color(0,60,80));
+		label.setFont(new Font("MV Boli", Font.BOLD, 20));
+		label.setIconTextGap(-25);
+		label.setBackground(Color.CYAN);
+		label.setOpaque(true);
+		label.setBorder(border);
+		label.setVerticalAlignment(JLabel.TOP);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		
+		// FRAME
+		frame.setTitle("POSEIDON");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setSize(440,380);
+		//frame.setLayout(null);
+		frame.setVisible(true);
+		frame.setIconImage(icon.getImage());
+
+		frame.add(label);
+		//frame.pack();
+		
+		// NOTIFICA DI ERRORE
+		JOptionPane.showMessageDialog(null,  "Carattere inserito non riconosciuto!", "ERRORE - POSEIDON", JOptionPane.WARNING_MESSAGE);
+		
+		// NOTIFICA DI INPUT 
+		String[] responses = {"No, sono un utente", "No, sono un cliente",  "Si"};
+		JOptionPane.showOptionDialog(null, "Sei un dipendente?", "POSEIDON", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, icon, responses, 0);
+		int dip_answer = JOptionPane.showConfirmDialog(null,  "Sei un dipendente?", "POSEIDON", JOptionPane.YES_NO_OPTION);
+		String nome= JOptionPane.showInputDialog("Inserisci il tuo nome: ");
+		String cognome= JOptionPane.showInputDialog("Inserisci il tuo cognome: ");
+		String password= JOptionPane.showInputDialog("Inserisci la password: ");
+		
+		// NOTIFICA DI CONFERMA
+		int answer = JOptionPane.showConfirmDialog(null,  "Sei sicuro di voler uscire?", "POSEIDON", JOptionPane.YES_NO_OPTION);
+		
+	}
+	
+
+}
--- src/poseidon/main/MyFrame.java
+++ src/poseidon/main/MyFrame.java
@@ -5,27 +5,33 @@ import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.border.Border;
 
-public class MyFrame extends JFrame {
+public class MyFrame extends JFrame implements ActionListener{
 	
 	private static final long serialVersionUID = 1L;
+	JButton button;
 
 	MyFrame() {
 		
 		ImageIcon icon = new ImageIcon("icon.jpeg");
 		Border border = BorderFactory.createLineBorder(Color.BLUE,4);
 			
+		// BACKGROUND
 		setLayout(new BorderLayout());
 		JLabel background =new JLabel(new ImageIcon("background.jpeg"));
 		this.add(background);
 		background.setLayout(new FlowLayout());	    
 		
+		// LABEL
 		JLabel label = new JLabel();
 		label.setText("POSEIDON");
 		label.setIcon(icon);
@@ -40,18 +46,35 @@ public class MyFrame extends JFrame {
 		label.setVerticalAlignment(JLabel.TOP);
 		label.setHorizontalAlignment(JLabel.CENTER);
 		
+		// BOTTON
+		button = new JButton();
+		button.addActionListener(this);
+		button.setText("START");
+		button.setFocusable(false);
+		button.setFont(new Font("MV Boli", Font.ITALIC, 15));
+		button.setForeground(Color.BLUE);
+		button.setForeground(Color.BLUE);
+		button.setBounds(0,0,100,50);			//TODO: Leonardo
+		button.setEnabled(true);
+		
+		// FRAME
 		this.setTitle("POSEIDON");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		//this.setSize(800,800);
-
 		this.setVisible(true);
 		this.setIconImage(icon.getImage());
 
 		background.add(label);
-	    
+		background.add(button);
 		this.pack();
 		
 	}
 
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == button) {
+			LaunchMenu launchMenu = new LaunchMenu();
+			button.setEnabled(false);
+		}
+	}
 }
